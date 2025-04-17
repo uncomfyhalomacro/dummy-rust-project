e64c153cc5abaad575dfdad012651a0da9558220
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-04-17 12:49:11 +0800

    ci: check formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use dummy_rust_project::hello;
 
 fn main() {
-    let unused_var: i32;
-    let say_hello = hello("Stackie");
-    println!("{}", say_hello);
+let                        unused_var: i32;
+    let say_hello =
+
+
+
+        hello("Stackie")            ;
+    println!(
+        "{}",
+
+
+
+            say_hello);
 }
