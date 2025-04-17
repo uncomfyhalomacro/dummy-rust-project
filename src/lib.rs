a568160041d1007e35ca6b5954e74d394e742579
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-04-17 15:16:41 +0800

    ci: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,19 @@ pub fn goodbye(name: &str) -> String {
     let goodbye_string = format!("Goodbye, {}!", name);
     goodbye_string
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn hello_stackie_matches() {
+        let hello_string = hello("Stackie");
+        assert_eq!("Hello, Stacker!", &hello_string)
+    }
+
+    #[test]
+    fn goodbye_stackie_matches() {
+        let goodbye_string = goodbye("Stackie");
+        assert_eq!("Goodbye, Stackie!", &goodbye_string)
+    }
+}
