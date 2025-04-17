b842be5db5b7cc951a98c704a1fb9e061c8dab63
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-04-17 12:36:26 +0800

    ci: make ci green
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,4 @@ fn main() {
     let unused_var: i32;
     let say_hello = hello("Stackie");
     println!("{}", say_hello);
-    let vec: Vec<isize> = Vec::new();
-    if vec.len() <= 0 {}
-    if 100 > i32::MAX {}
 }
