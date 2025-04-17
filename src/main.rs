445b17f4d0e82733897318a0e17289e4245eae65
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-04-17 11:25:04 +0800

    ci: make Run clippy fail
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,7 @@ fn main() {
     let unused_var: i32;
     let say_hello = hello("Stackie");
     println!("{}", say_hello);
+    let vec: Vec<isize> = Vec::new();
+    if vec.len() <= 0 {}
+    if 100 > i32::MAX {}
 }
